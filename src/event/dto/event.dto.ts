0b90c40779b6774679296b5155d6b62c3b0d5cf2
Marek Sokol <devmareksokol@gmail.com>
2022-11-06 01:04:14 +0100

    feat: note DTO & event DTO

--- src/event/dto/event.dto.ts
+++ src/event/dto/event.dto.ts
@@ -1,6 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsString, MinLength } from 'class-validator';
+
 export class EventDto {
+  @IsString()
+  @MinLength(4)
+  @ApiProperty({ type: String, description: 'title' })
   title: string;
+
+  @IsString()
+  @MinLength(4)
+  @ApiProperty({ type: String, description: 'content' })
   content: string;
+
+  @IsDate()
+  @ApiProperty({ type: Date, description: 'start' })
   start: Date;
+
+  @IsDate()
+  @ApiProperty({ type: Date, description: 'end' })
   end: Date;
 }
--- src/note/dto/note.dto.ts
+++ src/note/dto/note.dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
 export class NoteDto {
+  @IsString()
+  @MinLength(4)
+  @ApiProperty({ type: String, description: 'title' })
   title: string;
+
+  @IsString()
+  @MinLength(4)
+  @ApiProperty({ type: String, description: 'content' })
   content: string;
 }
